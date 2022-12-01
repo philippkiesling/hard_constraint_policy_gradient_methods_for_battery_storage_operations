9e163f98b3e860d88bc460dcf6dd70abfa30135a
philippkiesling <philipp.kiesling@student.kit.edu>
2022-12-01 10:59:22 +0100

    Adapt test_environment.py

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,7 +1,7 @@
 import unittest
-from src.environment import Environment
+from batterytrading.environment import Environment
 import numpy as np
-from src.models import Day_Ahead_Trader
+from batterytrading.rl_models import Day_Ahead_Trader
 
 
 class TestEnvironment(unittest.TestCase):
@@ -19,15 +19,15 @@ class TestEnvironment(unittest.TestCase):
         for i in range((24+12)*4):
             action = (np.random.random(1) - 0.5) * 0.3
             next_state, reward, done, info = self.environment.step(action)
-            reset_files.append(next_state["day-ahead-price"])
+            reset_files.append(next_state)
 
         self.environment.reset()
-        self.assertEqual( self.environment.data_loader.current_index, 0)
+        self.assertEqual(self.environment.data_loader.current_index, 1)
 
         for i in range(12*4+1, (24+12)*4):
             action = (np.random.random(1) - 0.5) * 0.3
             next_state, reward, done, info = self.environment.step(action)
-            #self.assertEqual(np.sum(reset_files[(12*41+1+i)]), np.sum(next_state["day-ahead-price"]))
+            # self.assertEqual(np.sum(reset_files[(12*41+1+i)]), np.sum(next_state["day-ahead-price"]))
 
     def test_dataloader(self):
         """
@@ -38,32 +38,11 @@ class TestEnvironment(unittest.TestCase):
         """
         dataloader = self.environment.data_loader
         done = False
-        for i in range(3):
+        for i in range(2):
             while not done:
-                day_ahead_price, intraday_price, done = dataloader.get_next_day_ahead_and_intraday_price()
-                dataloader.reset()
+                day_ahead_price, intraday_price, time_features, done = dataloader.get_next_day_ahead_and_intraday_price()
+            dataloader.reset()
 
-
-    def test_day_ahead_trader(self):
-        """
-        Test the
-        Returns:
-
-        """
-        env = self.environment
-        trader = Day_Ahead_Trader()
-        env.reset()
-        action = 0
-        sum_of_rewards = 0
-        while True:
-            next_state, reward, done, info = env.step(action)
-
-            if done:
-                break
-            action = trader.step(next_state, reward)
-            sum_of_rewards += reward
-        self.environment.reset()
-        self.assertGreater(sum_of_rewards, 100)
 
 if __name__ == '__main__':
     unittest.main()
