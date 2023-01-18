077a77e40e0f2c24b377ca85a26ecc3e9a2c646c
philippkiesling <philipp.kiesling@student.kit.edu>
2023-01-18 16:58:46 +0100

    Add Pretraining and RPPO-Policy handling to model_setup.py

--- batterytrading/ppo/model_setup.py
+++ batterytrading/ppo/model_setup.py
@@ -5,12 +5,13 @@ from sys import gettrace as sys_gettrace
 
 import yaml
 from pathlib import Path
-from batterytrading.environment import Environment, NormalizeObservationPartially
+from batterytrading.environment import Environment, NormalizeObservationPartially, RandomSamplePretrainingEnv
 import wandb
 from wandb.integration.sb3 import WandbCallback
 import torch.nn as nn
 import gym
 import numpy as np
+from batterytrading.baselines import BaselineModel
 
 def get_config(config_path):
     """
@@ -48,11 +49,16 @@ def get_config(config_path):
     # create environment and add it to the model config
     if model_config["env"] == "BatteryStorageEnv":
         env_config["wandb_run"] = run
-        model_config["env"] = _get_configured_env(env_config)
+        env = _get_configured_env(env_config)
+        model_config["env"] = env
+    if model_config["pretrain"] == True:
+        model_config["pretrain_env"] = _get_pretrained_env(env_config)
+    else:
+        model_config.pop("pretrain")
 
     # Convert learning rate to float
     if model_config["learning_rate"] == "schedule":
-        model_config["learning_rate"] = lambda f: f * 3e-4
+        model_config["learning_rate"] = lambda f: f * 2.5e-4
     else:
         try:
             model_config["learning_rate"] = float(model_config["learning_rate"])
@@ -61,29 +67,70 @@ def get_config(config_path):
 
     # resolve activation function
     model_config["policy_kwargs"]["activation_fn"] = _resolve_activation_function(model_config["policy_kwargs"]["activation_fn"])
-    if "lstm" in model_config["policy"].lower():
-        model_config["policy_type"] = "LSTM"
+    # resolve policy:
+    model_config = _resolve_policy(model_config, env_config)
+    # setup pretraining DEPRICATED PRETRAINING
+    # model_config["policy_kwargs"]["pretrain"] = _setup_pretraining(model_config["policy_kwargs"]["pretrain"], env_config)
+
+    return model_config, train_config
+
+def _resolve_policy(model_config, env_config):
+    """
+    resolves policy from string. Parses polcy_kwargs in some cases to get correct policy type.
+    Args:
+        model_config: config for model
+        env_config: config for environment (needed for policy_kwargs oflinear projected)
+
+    Returns:
+
+    """
+    policy = model_config["policy"].lower()
+    if "clampedlstm" in policy:
+        model_config["policy_type"] = "ClampedMlpLstmPolicy"
+        lstm_kwargs = model_config["policy_kwargs"].pop("lstm_kwargs")
+        model_config["policy_kwargs"]["env_reference"] = model_config["env"]
+
+        for key in lstm_kwargs:
+            model_config["policy_kwargs"][key] = lstm_kwargs[key]
+        model_config["policy_kwargs"]
+        model_config["policy_kwargs"]["bounds"] = (- env_config["max_charge"], env_config["max_charge"])
+    elif "linearprojectedlstm" in policy:
+        model_config["policy_type"] = "LinearProjectedMlpLstmPolicy"
+        model_config["policy_kwargs"]["env_reference"] = model_config["env"]
+
+        lstm_kwargs = model_config["policy_kwargs"].pop("lstm_kwargs")
+        for key in lstm_kwargs:
+            model_config["policy_kwargs"][key] = lstm_kwargs[key]
+            model_config["policy_kwargs"]
+            model_config["policy_kwargs"]["bounds"] = (- env_config["max_charge"], env_config["max_charge"])
+    elif "lstm" in policy:
+        model_config["policy_type"] = "MlpLstmPolicy"
         lstm_kwargs = model_config["policy_kwargs"].pop("lstm_kwargs")
         for key in lstm_kwargs:
             model_config["policy_kwargs"][key] = lstm_kwargs[key]
         model_config["policy_kwargs"]
-    elif "clampedmlp" in model_config["policy"].lower():
+
+    elif "clampedmlp" in policy:
         model_config["policy_type"] = "ClampedMlpPolicy"
+
         model_config["policy_kwargs"].pop("lstm_kwargs")
         model_config["policy_kwargs"]["bounds"] = (- env_config["max_charge"], env_config["max_charge"])
-    elif "linearprojected" in model_config["policy"].lower():
+
+    elif "linearprojected" in policy:
         model_config["policy_type"] = "LinearProjectedMlpPolicy"
         model_config["policy_kwargs"].pop("lstm_kwargs")
         model_config["policy_kwargs"]["bounds"] = (- env_config["max_charge"], env_config["max_charge"])
-    elif "linear" in model_config["policy"].lower():
+
+    elif "linear" in policy:
         model_config["policy_type"] = "LinearPolicy"
         model_config["policy_kwargs"].pop("lstm_kwargs")
         model_config["policy_kwargs"].pop("activation_fn")
+
     else:
-        model_config["policy_type"] = "MLP"
+        model_config["policy_type"] = "MlpPolicy"
         model_config["policy_kwargs"].pop("lstm_kwargs")
-    # Set action bounds to valid values
-    return model_config, train_config
+
+    return model_config
 
 def _resolve_activation_function(activation_fn):
     """
@@ -112,6 +159,8 @@ def _resolve_activation_function(activation_fn):
         return nn.GELU
     else:
         raise ValueError("activation function not implemented")
+global env_preprocessing
+env_preprocessing = None
 
 def _get_configured_env(env_config):
     """
@@ -121,9 +170,40 @@ def _get_configured_env(env_config):
     Returns:
         env (object): environment
     """
-    env_preprocessing = env_config.pop("preprocessing")
+    global env_preprocessing
+    if env_preprocessing is None:
+        env_preprocessing = env_config.pop("preprocessing")
 
     env = Environment(**env_config)
+    if env_preprocessing["clipaction"]:
+        env = gym.wrappers.ClipAction(env)
+    if env_preprocessing["normalizeobservation"]:
+        env = gym.wrappers.NormalizeObservation(env)
+
+        #env = NormalizeObservationPartially(env)
+    if env_preprocessing["normalizereward"]:
+        env = gym.wrappers.NormalizeReward(env)
+    if env_preprocessing["transformobservation"]:
+        env = gym.wrappers.TransformObservation(env, lambda x: x.flatten())
+        # env = gym.wrappers.TransformObservation(env, lambda x: np.clip(x[0].flatten(), -10, 10))
+    if env_preprocessing["transformreward"]:
+        #env = gym.wrappers.TransformReward(env, lambda x: x.flatten())
+        env = gym.wrappers.TransformReward (env, lambda x: np.clip(x, -10, 10))
+    return env
+
+def _get_pretrained_env(env_config):
+    """
+    creates environment with given config
+    Args:
+        env_config (dict): config for environment
+    Returns:
+        env (object): environment
+    """
+    global env_preprocessing
+    if env_preprocessing is None:
+        env_preprocessing = env_config.pop("preprocessing")
+
+    env = RandomSamplePretrainingEnv(**env_config)
     if env_preprocessing["clipaction"]:
         env = gym.wrappers.ClipAction(env)
     if env_preprocessing["normalizeobservation"]:
@@ -138,6 +218,34 @@ def _get_configured_env(env_config):
         #env = gym.wrappers.TransformReward(env, lambda x: x.flatten())
         env = gym.wrappers.TransformReward (env, lambda x: np.clip(x, -10, 10))
     return env
+
+
+def _setup_pretraining(pretrain_config, env_config):
+    """
+    sets up pretraining configuration of model, returns dict or None
+
+    Args:
+        pretrain_config (dict): config for model
+    Returns:
+        pretrain_config (dict): config for model
+    """
+    if pretrain_config.pop("pretrain") == False:
+        return None
+    else:
+
+        env = _get_configured_env(env_config)
+        learnable_env_cfg = env_config.copy()
+        #learnable_env_cfg["day_ahead_environment"] = True
+        learnable_env = Environment(**learnable_env_cfg)
+        pretrain_config["env"] = env
+        pretrain_config["learnable_env"] = learnable_env
+        teacher_policy = BaselineModel(learnable_env)
+        pretrain_config["teacher_policy"] = teacher_policy
+        return pretrain_config
+
+
+
+        return pretrain_config
 
 if __name__ == "__main__":
     config = get_config("cfg.yml")
